9f1f489e97dd0f618f88cd231b7840d979dd7d3f
Mason Stallmo <masonstallmo@gmail.com>
2018-11-18 11:50:43 -0600

    First gstreamer tutorial in Rust

---
+++ gstreamer-tutorial-1/src/main.rs
@@ -0,0 +1,38 @@
+#![feature(extern_crate_item_prelude)]
+
+extern crate gstreamer as gst;
+
+use gst::prelude::*;
+
+fn main() {
+    gst::init().unwrap();
+
+    let uri =
+        "https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm";
+    let pipeline = gst::parse_launch(&format!("playbin uri={}", uri)).unwrap();
+
+    let ret = pipeline.set_state(gst::State::Playing);
+    assert_ne!(ret, gst::StateChangeReturn::Failure);
+
+    let bus = pipeline.get_bus().unwrap();
+    while let Some(msg) = bus.timed_pop(gst::CLOCK_TIME_NONE) {
+        use gst::MessageView;
+
+        match msg.view() {
+            MessageView::Eos(..) => break,
+            MessageView::Error(err) => {
+                println!(
+                    "Error from {:?}: {} ({:?})",
+                    err.get_src().map(|s| s.get_path_string()),
+                    err.get_error(),
+                    err.get_debug()
+                );
+                break;
+            }
+            _ => (),
+        }
+    }
+
+    let ret = pipeline.set_state(gst::State::Null);
+    assert_ne!(ret, gst::StateChangeReturn::Failure);
+}
